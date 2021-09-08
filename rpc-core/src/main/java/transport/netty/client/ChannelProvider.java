06b615aa23dce45e576e319870469d743232c06d
william.Liu <893051689@qq.com>
2021-09-08 14:52:29 +0800

    进一步完善Netty客户端和服务器端

--- rpc-core/src/main/java/transport/netty/client/ChannelProvider.java
+++ rpc-core/src/main/java/transport/netty/client/ChannelProvider.java
@@ -1,4 +1,85 @@
 package transport.netty.client;
 
+import codec.CommonDecoder;
+import codec.CommonEncoder;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
+import serializer.CommonSerializer;
+
+import java.net.InetSocketAddress;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 获取Channel
+ */
 public class ChannelProvider {
+
+    private static EventLoopGroup eventLoopGroup;
+    private static Bootstrap bootstrap = initializeBootstrap();
+    private static Map<String,Channel> channels = new ConcurrentHashMap<>();
+
+    public static Channel get(InetSocketAddress inetSocketAddress, CommonSerializer serializer){
+        //将socket地址和序列化器编码作为channel的唯一标识符
+        String key = inetSocketAddress.toString() + serializer.getCode();
+        //如果该Channel已经存在，直接返回
+        if(channels.containsKey(key)){
+            Channel channel = channels.get(key);
+            if(channels!=null && channel.isActive()){
+                return channel;
+            }else {
+                channels.remove(key);
+            }
+        }
+        bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel socketChannel) throws Exception {
+                socketChannel.pipeline().addLast(new CommonEncoder(serializer))
+                        .addLast(new IdleStateHandler(0,5,0, TimeUnit.SECONDS))
+                        .addLast(new CommonDecoder())
+                        .addLast(new NettyClientHandler());
+            }
+        });
+        Channel channel = null;
+        try {
+            channel = connect(bootstrap,inetSocketAddress);
+        }catch (ExecutionException | InterruptedException e){
+            e.printStackTrace();
+            return null;
+        }
+        channels.put(key,channel);
+        return channel;
+    }
+
+    private static Channel connect(Bootstrap bootstrap, InetSocketAddress inetSocketAddress) throws ExecutionException, InterruptedException {
+        CompletableFuture<Channel> completableFuture = new CompletableFuture<>();
+        bootstrap.connect(inetSocketAddress).addListener((ChannelFutureListener) future -> {
+            if(future.isSuccess()){
+                completableFuture.complete(future.channel());
+            }else {
+                throw new IllegalStateException();
+            }
+        });
+        return completableFuture.get();
+    }
+
+
+    private static Bootstrap initializeBootstrap(){
+        eventLoopGroup = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(eventLoopGroup)
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS,5000)
+                .option(ChannelOption.SO_KEEPALIVE,true)
+                .option(ChannelOption.TCP_NODELAY,true);
+        return bootstrap;
+    }
+
 }
--- rpc-core/src/main/java/transport/netty/client/NettyClient.java
+++ rpc-core/src/main/java/transport/netty/client/NettyClient.java
@@ -1,4 +1,84 @@
 package transport.netty.client;
 
-public class NettyClient {
+import entity.RpcRequest;
+import entity.RpcResponse;
+import enumeration.RpcError;
+import exception.RpcException;
+import factory.SingletonFactory;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import loadbalancer.LoadBalancer;
+import loadbalancer.RandomLoadBalancer;
+import registry.NacosServiceDiscovery;
+import registry.ServiceDiscovery;
+import serializer.CommonSerializer;
+import transport.netty.RpcClient;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.CompletableFuture;
+
+public class NettyClient implements RpcClient {
+
+    private static final EventLoopGroup group;
+    private static final Bootstrap bootstrap;
+    private final ServiceDiscovery serviceDiscovery;
+    private final CommonSerializer serializer;
+    private final UnprocessedRequests unprocessedRequests;
+
+    public NettyClient() {
+        this(DEFAULT_SERIALIZER, new RandomLoadBalancer());
+    }
+    public NettyClient(LoadBalancer loadBalancer) {
+        this(DEFAULT_SERIALIZER, loadBalancer);
+    }
+    public NettyClient(Integer serializer) {
+        this(serializer, new RandomLoadBalancer());
+    }
+    public NettyClient(Integer serializer, LoadBalancer loadBalancer) {
+        this.serviceDiscovery = new NacosServiceDiscovery(loadBalancer);
+        this.serializer = CommonSerializer.getByCode(serializer);
+        this.unprocessedRequests = SingletonFactory.getInstance(UnprocessedRequests.class);
+    }
+
+    static {
+        group = new NioEventLoopGroup();
+        bootstrap = new Bootstrap();
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class);
+    }
+
+    @Override
+    public Object sendRequest(RpcRequest rpcRequest) {
+        if (serializer == null) {
+            throw new RpcException(RpcError.SERIALIZER_NOT_FOUND);
+        }
+        CompletableFuture<RpcResponse> future = new CompletableFuture<>();
+        try {
+            //首先根据接口名找到socket地址
+            InetSocketAddress inetSocketAddress = serviceDiscovery.lookupService(rpcRequest.getInterfaceName());
+            //然后根据socket地址找到对应的channel
+            Channel channel = ChannelProvider.get(inetSocketAddress,serializer);
+            if(!channel.isActive()){
+                group.shutdownGracefully();
+                return null;
+            }
+            unprocessedRequests.put(rpcRequest.getRequestId(), future);
+            channel.writeAndFlush(rpcRequest).addListener((ChannelFutureListener)future1 ->{
+                if(future1.isSuccess()){
+                    System.out.println("发送消息成功");
+                }else {
+                    future1.channel().close();
+                    future.completeExceptionally(future1.cause());
+                }
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return future;
+    }
+
 }
